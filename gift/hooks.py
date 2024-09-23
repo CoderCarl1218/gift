d3967f6b50dc115e649f2c3901ac3d06676f33f6
Carl <carl@servio.ph>
2024-09-23 16:42:09 +0800

    Your new commit message

--- gift/hooks.py
+++ gift/hooks.py
@@ -28,6 +28,7 @@ app_license = "mit"
 # app_include_css = "/assets/gift/css/gift.css"
 # app_include_js = "/assets/gift/js/gift.js"
 
+
 # include js, css files in header of web template
 # web_include_css = "/assets/gift/css/gift.css"
 # web_include_js = "/assets/gift/js/gift.js"
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./gift/wwww/**/*.html",
+    "./gift/templates/**/*.html",
+    ".gift/public/**/*.html",
+    "..gift/public/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
